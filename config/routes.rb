4697d37b0edec5e9cd6c67f2b6c27a9247ebabf3
Williamsliam23 <williams.liam23@gmail.com>
2021-11-26 15:04:43 -0600

    css reset

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :delete, :show, :index]
-    resource :session, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :delete, :show]
+    # resources messages index show delete create update
   end
 
   root "static_pages#root"
