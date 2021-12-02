1c6d67e445e26b227179b1394944342b27e3da45
Williamsliam23 <williams.liam23@gmail.com>
2021-12-02 08:40:39 -0600

    seperate server and channel for future components

---
+++ frontend/components/channels/channel_list.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class ChannelList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+
+    return (
+      
+      <div className='channel-wrap'>
+        <h3><a>Channel 1</a></h3>
+      </div>
+        
+    )
+  }
+}
+
+export default ChannelList
---
+++ frontend/components/servers/server_list.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class ServerList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+
+    return (
+      
+      <div className='server-wrap'>
+        <h3><a>Main</a></h3>
+      </div>
+        
+    )
+  }
+}
+
+export default ServerList
