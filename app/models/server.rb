5b368f59623c6068096504475f9a2b8779b6ebf5
Williamsliam23 <williams.liam23@gmail.com>
2021-12-24 20:06:02 -0600

    channel and server model basic validations

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,2 +1,10 @@
 class Server < ApplicationRecord
+
+  validates :author_id, :title, presence: true
+
+  belongs_to :author,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: :User
+
 end
