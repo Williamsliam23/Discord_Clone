1ae5c95eec46130374bfa56bbdd972e061efdf75
Williamsliam23 <williams.liam23@gmail.com>
2022-01-13 17:30:09 -0600

    channel creation form

--- frontend/components/channels/channel_form.jsx
+++ frontend/components/channels/channel_form.jsx
@@ -1,1 +1,41 @@
-// 
+import React from "react";
+
+class ChannelForm extends React.Component {
+
+  constructor(props) {
+      super(props)
+      let setTitle, setAuthor;
+      setAuthor = this.props.currentUser;
+      setTitle = ""
+      this.state = {
+          title: setTitle,
+          authorId: setAuthor
+      }
+      this.handleSubmit = this.handleSubmit.bind(this)
+      this.updateChannel = this.updateChannel.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+      const channel = Object.assign({}, this.state);
+      this.props.processCreate(channel)
+      this.setState({
+        title: "",
+        authorId: this.props.currentUser
+      })
+  }
+  
+  updateChannel(e) {
+      this.setState({title: e.currentTarget.value})
+  }
+  
+  render () {
+    return(
+      <form className='position-chat' onSubmit={this.handleSubmit}>
+          <input className="channel-name" type="text" value={this.state.title} onChange={this.updateChannel}/>
+          <input className="create-channel" type="submit" value="Submit"/>
+      </form>
+    )
+  }
+}
+
+export default ChannelForm
